7e0c5fbc913eb2026b1cb2b5fd11829d77325008
Artem Smilhain <asmilhain@activit.sk>
2023-04-30 00:39:17 +0200

    huj

--- index.php
+++ index.php
@@ -11,11 +11,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     // Получение переменных из ссылки
     $temperature = $_GET['temperature'];
     $humidity = $_GET['humidity'];
-    $recorded_at = date('Y-m-d H:i:s');
+    $created_at = date('Y-m-d H:i:s');
 
-    $query = "INSERT INTO meteo (temperature, humidity, date) VALUES (?, ?, ?)";
+    $query = "INSERT INTO meteo (temperature, humidity, created_at) VALUES (?, ?, ?)";
     $stmt = mysqli_prepare($db, $query);
-    mysqli_stmt_bind_param($stmt, 'dds', $temperature, $humidity, $recorded_at);
+    mysqli_stmt_bind_param($stmt, 'dds', $temperature, $humidity, $created_at);
     mysqli_stmt_execute($stmt);
 
     mysqli_stmt_close($stmt);
