aeee5b902599b4511f4a21fdea855a46f1048746
Artem Smilhain <asmilhain@activit.sk>
2023-04-30 00:37:05 +0200

    index.php

--- index.php
+++ index.php
@@ -1,1 +1,26 @@
 <?php
+
+$db = mysqli_connect("viibes_zct", "viibes_zct", "#R~vDj3b77", "viibes_zct");
+
+if (mysqli_connect_errno()) {
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    exit();
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    // Получение переменных из ссылки
+    $temperature = $_GET['temperature'];
+    $humidity = $_GET['humidity'];
+    $recorded_at = date('Y-m-d H:i:s');
+
+    $query = "INSERT INTO meteo (temperature, humidity, date) VALUES (?, ?, ?)";
+    $stmt = mysqli_prepare($db, $query);
+    mysqli_stmt_bind_param($stmt, 'dds', $temperature, $humidity, $recorded_at);
+    mysqli_stmt_execute($stmt);
+
+    mysqli_stmt_close($stmt);
+}
+
+mysqli_close($db);
+
+?>
